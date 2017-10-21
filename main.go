8d24824403a14a58a04a7bafe4bd4ae8feea284f
Pat Martin <23747724+ravenawk@users.noreply.github.com>
2017-10-21 15:47:14 -0700

    Clean up

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	//	var colors map[string]string
 
-	colors := make(map[string]string)
-
-	//	colors := map[string]string{
-	//		"red":   "#ff0000",
-	//		"green": "#00ff00",
-	//	}
-
-	colors["white"] = "#ffffff"
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
 
 	fmt.Println(colors)
 
