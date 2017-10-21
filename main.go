17241fbb3eb2d8386c721e0ddb7469726ac60963
Pat Martin <23747724+ravenawk@users.noreply.github.com>
2017-10-21 15:43:29 -0700

    First commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	var colors map[string]string
+
+	colors := make(map[string]string)
+
+	//	colors := map[string]string{
+	//		"red":   "#ff0000",
+	//		"green": "#00ff00",
+	//	}
+
+	colors["white"] = "#ffffff"
+
+	fmt.Println(colors)
+
+}
